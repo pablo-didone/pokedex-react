e112eddcc7341743be8fcd0232f59fa052a34a67
Pablo <padidone@gmail.com>
2018-01-31 01:22:20 +0700

    add flexbox grid

--- src/pages/list/components/pokemon-list.js
+++ src/pages/list/components/pokemon-list.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PokemonItem from './pokemon-item';
+import './pokemon-list.scss';
 
 const PokemonList = (props) => (
-  <div className="">
-    <PokemonItem />
+  <div className="Pokemon-list">
+  {
+    [1,2,3,4,5,6,7,8,9,10,11].map((e, i) => (
+      <PokemonItem />
+    ))
+  }
   </div>
 );
 
